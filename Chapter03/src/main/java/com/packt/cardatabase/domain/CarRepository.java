17ba5f65027a2e0d6638bd23b3f8352f9774fa34
Samuel Herrmann <samuelherrmann@gmx.net>
2022-07-23 15:33:14 +0100

    change mapping of CarRepository to /vehicles

--- Chapter03/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ Chapter03/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,6 +1,8 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="vehicles")
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
