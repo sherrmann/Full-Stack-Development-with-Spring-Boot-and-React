fc07081bcb3081d9f1fc2faf0d64a7562f47990d
Samuel Herrmann <samuelherrmann@gmx.net>
2022-07-25 13:11:48 +0100

    remove custom entity service path

--- Chapter03/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ Chapter03/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,8 +1,6 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="vehicles")
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
