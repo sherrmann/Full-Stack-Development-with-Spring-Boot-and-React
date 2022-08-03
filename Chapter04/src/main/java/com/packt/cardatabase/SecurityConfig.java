622fc4126ca70f5a75b6fec3a4d8cf62c7998eff
Samuel Herrmann <samuelherrmann@gmx.net>
2022-08-03 13:29:15 +0100

    change SecurityConfig, change logging level to INFO

--- Chapter04/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ Chapter04/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.packt.cardatabase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.packt.cardatabase.service.UserDetailsServiceImpl;
@@ -19,4 +24,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}	
+	
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception{
+		return authenticationManager();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		// disable CSRF
+		http.csrf().disable()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeRequests()
+		// POST request to /login endpoint is not secured
+		.antMatchers(HttpMethod.POST, "/login")
+		.permitAll()
+		// all other requests are secured
+		.anyRequest().authenticated();
+	}
 }
