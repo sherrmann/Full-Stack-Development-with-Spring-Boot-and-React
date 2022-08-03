bdb0e9b199378ac6840d45ebdc60f0c872c2f674
Samuel Herrmann <samuelherrmann@gmx.net>
2022-08-03 18:58:02 +0100

    add Authentication to SecurityContextHolder

--- Chapter04/src/main/java/com/packt/cardatabase/AuthenticationFilter.java
+++ Chapter04/src/main/java/com/packt/cardatabase/AuthenticationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -33,6 +34,8 @@ public class AuthenticationFilter extends OncePerRequestFilter{
 			
 			// Authenticate
 			Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, java.util.Collections.emptyList());
+			
+			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
 		
 		filterChain.doFilter(request, response);
