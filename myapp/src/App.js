14d2d4f3e09796485c54111233980cb63f63475b
Samuel Herrmann <samuelherrmann@gmx.net>
2022-08-05 14:13:30 +0100

    add comments, change default page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return <h1>second try</h1>
-  }
+function App(){
+  return (
+    <div>
+    <h1>create comonents using functions</h1>
+    <h2>put both elements into an unnamed div</h2>
+    </div>
+  )
 }
 
 export default App;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Create a root.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Render an element to the root
 root.render(
   <React.StrictMode>
     <App />
