1203ea2fb170c012bfb6a6b3fcdea8103cb5a305
Samuel Herrmann <samuelherrmann@gmx.net>
2022-08-27 11:07:14 +0100

    remove security lol

--- Chapter04/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ Chapter04/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -58,18 +58,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		// disable CSRF
-		http.csrf().disable().cors().and()
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-		.authorizeRequests()
-		// POST request to /login endpoint is not secured
-		.antMatchers(HttpMethod.POST, "/login")
-		.permitAll()
-		// all other requests are secured
-		.anyRequest().authenticated().and()
-		.exceptionHandling()
-		.authenticationEntryPoint(exceptionHandler).and()
-		.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+		
+		/*
+		 * http.csrf().disable().cors().and() .sessionManagement()
+		 * .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		 * .authorizeRequests() // POST request to /login endpoint is not secured
+		 * .antMatchers(HttpMethod.POST, "/login") .permitAll() // all other requests
+		 * are secured .anyRequest().authenticated().and() .exceptionHandling()
+		 * .authenticationEntryPoint(exceptionHandler).and()
+		 * .addFilterBefore(authenticationFilter,
+		 * UsernamePasswordAuthenticationFilter.class);
+		 */
 	}
 }
